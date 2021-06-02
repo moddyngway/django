11026dfb4bac5500de16a7e8aaa0db73fd2bb3ce
moddyngway <duyshenlievadil02@gmail.com>
2021-06-03 00:27:40 +0600

    test

--- main/urls.py
+++ main/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="home"),
-    path('about', views.about, name="about-us"),
-    path('fumo', views.fumo, name="fumo"),
-    path('create', views.create, name="create"),
+    path('', views.TaskListView.as_view(), name="home"),
+    path('about/', views.about, name="about-us"),
+    path('fumo/', views.fumo, name="fumo"),
+    path('create/', views.create, name="create"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render, redirect
+from django.views.generic import ListView, CreateView
+
 from .models import Task
 from .forms import TaskForm
 
 
-def index(request):
-    tasks = Task.objects.all()
-    context = {
-        "title": "moddyngway's site",
-        "tasks": tasks
-    }
-    return render(request, "main/index.html", context)
+# def index(request):
+#     tasks = Task.objects.all()
+#     context = {
+#         "title": "moddyngway's site",
+#         "tasks": tasks
+#     }
+#     return render(request, "main/index.html", context)
 
 
 def about(request):
@@ -25,10 +27,18 @@ def create(request):
 
     form = TaskForm()
     context = {
-        "form" : form
+        "form": form
     }
     return render(request, "main/create.html", context)
 
 
 def fumo(request):
     return render(request, "main/fumo.html")
+
+
+class TaskListView(ListView):
+    model = Task
+    template_name = "main/index.html"
+    context_object_name = "tasks"
+
+
